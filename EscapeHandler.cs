64144f17a180de11289f936064492c067a976a91
NotIntense <mbro8210@gmail.com>
2023-10-08 00:54:00 -0400

    Add project files.

---
+++ EscapeHandler.cs
@@ -0,0 +1,45 @@
+﻿namespace CuffChanges
+{
+    using Exiled.API.Extensions;
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs.Player;
+    using PlayerRoles;
+
+    public static class EscapeHandler
+    {
+        public static Config? Config = null;
+
+        public static void Escape(EscapingEventArgs ev)
+        {
+            if (ev.EscapeScenario != Exiled.API.Enums.EscapeScenario.CustomEscape)
+                return;
+
+            if (Config.RoleConversionDictionary.TryGetValue(ev.Player.Role.Type, out RoleConversionInfo? roleConversionInfo))
+            {
+                if (roleConversionInfo.NeedToBeCuffed && !ev.Player.IsCuffed)
+                    return;
+
+                ev.NewRole = roleConversionInfo.TargetRole;
+                ev.IsAllowed = true;
+
+                if (Config.DisplayCustomEscapeMessage && !string.IsNullOrEmpty(roleConversionInfo.Message))
+                {
+                    DisplayEscapeMessage(ev.Player, roleConversionInfo.Message, roleConversionInfo.TargetRole);
+                }
+            }
+        }
+
+        public static void DisplayEscapeMessage(Player player, string message, RoleTypeId newRole) 
+        {
+            string edit = message.Replace("{newRole}", $"<color={newRole.GetColor().ToHex()}>{newRole.GetFullName()}</color>");
+            player.Broadcast(new Broadcast(edit, 5), true);
+        }
+    }
+
+    public class RoleConversionInfo
+    {
+        public RoleTypeId TargetRole { get; set; }
+        public bool NeedToBeCuffed { get; set; } = false;
+        public string? Message { get; set; } = string.Empty;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿namespace CuffChanges
+{
+    using Exiled.API.Features;
+    using System;
+    using PlayerHandler = Exiled.Events.Handlers.Player;
+
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "CuffChanges";
+        public override string Author { get; } = "NotIntense";
+        public override string Prefix { get; } = "CF";
+        public override Version Version { get; } = new(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new(8, 2, 0);
+
+        public override void OnEnabled()
+        {         
+            EscapeHandler.Config = Config;
+            PlayerHandler.Escaping += EscapeHandler.Escape;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            PlayerHandler.Escaping -= EscapeHandler.Escape;
+            EscapeHandler.Config = null;
+            base.OnDisabled();
+        }
+    }
+}
